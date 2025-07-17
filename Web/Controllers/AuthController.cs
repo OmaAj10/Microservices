aa5399bf33cb5291b2fbadbe78d5834d97461c6f
OmaAj10 <omar.m10@hotmail.com>
2025-07-17 19:09:50 +0200

    Register functionality works.

--- Web/Controllers/AuthController.cs
+++ Web/Controllers/AuthController.cs
@@ -1,6 +1,10 @@
+using System.Net.Mime;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Web.Models;
 using Web.Models.Dto;
 using Web.Service.IService;
+using Web.Utility;
 
 namespace Web.Controllers;
 
@@ -23,7 +27,46 @@ public class AuthController : Controller
     [HttpGet]
     public IActionResult Register()
     {
+        var roleList = new List<SelectListItem>()
+        {
+            new SelectListItem { Text = StaticDetails.RoleAdmin, Value = StaticDetails.RoleAdmin },
+            new SelectListItem { Text = StaticDetails.RoleCustomer, Value = StaticDetails.RoleCustomer }
+        };
+        
+        ViewBag.RoleList = roleList;
         return View();
+    } 
+    
+    [HttpPost]
+    public async Task<IActionResult> Register(RegistrationRequestDto registrationRequestDto)
+    {
+       ResponseDto responseDto = await _authService.RegisterAsync(registrationRequestDto);
+       ResponseDto assingRole;
+
+       if (responseDto != null && responseDto.IsSuccess)
+       {
+           if (string.IsNullOrEmpty(registrationRequestDto.Role))
+           {
+               registrationRequestDto.Role = StaticDetails.RoleCustomer;
+           }
+           assingRole = await _authService.AssignRoleAsync(registrationRequestDto);
+
+           if (assingRole != null && assingRole.IsSuccess)
+           {
+               TempData["success"] = "Registration Successful";
+               return RedirectToAction(nameof(Login));
+           }
+       }
+       
+       var roleList = new List<SelectListItem>()
+       {
+           new SelectListItem { Text = StaticDetails.RoleAdmin, Value = StaticDetails.RoleAdmin },
+           new SelectListItem { Text = StaticDetails.RoleCustomer, Value = StaticDetails.RoleCustomer }
+       };
+       
+       ViewBag.RoleList = roleList;
+       
+        return View(registrationRequestDto);
     }
     
     public IActionResult Logout()
