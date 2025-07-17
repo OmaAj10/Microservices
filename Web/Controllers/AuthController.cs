d3b06a6acfc3cfa19cc58066a5e53ebf427bc758
OmaAj10 <omar.m10@hotmail.com>
2025-07-17 18:08:47 +0200

    Implement auth service in web and views for login and register.

---
+++ Web/Controllers/AuthController.cs
@@ -0,0 +1,33 @@
+using Microsoft.AspNetCore.Mvc;
+using Web.Models.Dto;
+using Web.Service.IService;
+
+namespace Web.Controllers;
+
+public class AuthController : Controller
+{
+    private readonly IAuthService _authService;
+
+    public AuthController(IAuthService authService)
+    {
+        _authService = authService;
+    }
+
+    [HttpGet]
+    public IActionResult Login()
+    {
+        LoginRequestDto loginRequestDto = new();
+        return View(loginRequestDto);
+    }
+    
+    [HttpGet]
+    public IActionResult Register()
+    {
+        return View();
+    }
+    
+    public IActionResult Logout()
+    {
+        return View();
+    }
+}
